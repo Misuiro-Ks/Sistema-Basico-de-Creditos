3ada20cfc4fa14551f600f3201148e2170266f5f
unknown <fabian.ni.fr@gmail.com>
2026-07-29 14:31:19 -0500

    Navegacion entre Secciones y Configurar Tasa

--- simulador_completo.js
+++ simulador_completo.js
@@ -11,3 +11,33 @@
 
   
 //Para recuperar o mostrar información usar los métodos de la clase utilitarios, puede agregar métodos adicionales en utilitarios
+
+//Funcion ocultarSecciones
+function ocultarSecciones(){
+    let secciones = document.querySelectorAll("section");
+    for(let i = 0; i < secciones.length; i++){
+      let componente = secciones[i];
+      let listaClass = componente.classList;
+      listaClass.remove("activa");
+    }
+  }
+
+  function mostrarSeccion(id){
+    ocultarSecciones();
+
+    let componente = document.getElementById(id);
+    let listaClass = componente.classList;
+    listaClass.add("activa");
+  }
+
+  //Configurar Tasa
+  function guardarTasa(){
+    let valorIngresado = recuperarInt("tasaInteres");
+
+    if(valorIngresado >= 10 && valorIngresado <= 20){
+      tasaInteres = valorIngresado;
+      mostrarTexto("mensajeTasa", "Tasa configurada correctamente: " + valorIngresado + "%");
+    } else {
+      mostrarTexto("mensajeTasa", "La tasa debe estar entre 10% y 20%");
+    }
+  }
